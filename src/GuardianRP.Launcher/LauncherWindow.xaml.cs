76063115d29b42620095b384817337610bae941b
Filipsi <Filip.sikula@seznam.cz>
2017-05-22 22:38:57 +0200

    Referenced api submodule instead of compiled library

--- src/GuardianRP.Launcher/LauncherWindow.xaml.cs
+++ src/GuardianRP.Launcher/LauncherWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuardianRP.Api.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace GuardianRP.Launcher {
 
     public partial class MainWindow : Window {
 
-        private Api.Client.ApiClient client = new Api.Client.ApiClient("guardianrp.cz");
+        private ApiClient client = new ApiClient("guardianrp.cz");
 
         public MainWindow() {
             InitializeComponent();
